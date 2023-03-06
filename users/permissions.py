e9ae93394b7c1e8c84cadaa8900242aa6e9f43c6
Anibal <anibal4g@gmail.com>
2023-03-06 01:57:57 -0300

    entrega band kamp

--- users/permissions.py
+++ users/permissions.py
@@ -1,8 +1,17 @@
 from rest_framework import permissions
 from .models import User
-from rest_framework.views import View
+from rest_framework.views import View, Request
 
 
 class IsAccountOwner(permissions.BasePermission):
     def has_object_permission(self, request, view: View, obj: User) -> bool:
         return request.user.is_authenticated and obj == request.user
+
+
+class IsSuperOrReadOnly(permissions.BasePermission):
+    def has_permission(self, request: Request, view: View):
+        return (
+            request.method in permissions.SAFE_METHODS
+            or request.user.is_authenticated
+            and request.user.is_superuser
+        )
--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 from rest_framework_simplejwt import views as jwt_views
+from rest_framework_simplejwt.views import TokenObtainPairView
 
 urlpatterns = [
     path("users/", views.UserView.as_view()),
